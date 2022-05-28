b2b3851abf9f8fa983f292613a40625bbfea59dd
kcastroverde <castroverde.kevin@gmail.com>
2022-05-28 03:19:06 -0500

    hashroute

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import SideBar from "./components/Sidebar/SideBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter, Route, Routes } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import Tecnologias from "./pages/Tecnologias";
@@ -9,7 +9,7 @@ import Contactos from "./pages/Contactos";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <SideBar>
         <Routes>
           <Route path="/" element={<Dashboard />} />
@@ -20,7 +20,7 @@ function App() {
           <Route path="*" element={<> not found</>} />
         </Routes>
       </SideBar>
-    </Router>
+    </HashRouter>
   );
 }
 
